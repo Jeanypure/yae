8df4efed692c9604c0dec750e9bba0c519299a5f
Jenny <m17721480297@163.com>
2018-05-07 21:00:45 +0800

    pur-info module

---
+++ backend/models/PurInfo.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "pur_info".
+ *
+ * @property int $pur_info_id 主键
+ * @property int $pur_responsible_id 开发负责人
+ * @property int $pur_group 序号
+ * @property string $pd_title 中文简称
+ * @property string $pd_title_en 英文全称
+ * @property string $pd_pic_url 图片
+ * @property string $pd_package 外包装
+ * @property string $pd_length 长cm
+ * @property string $pd_width 宽cm
+ * @property string $pd_height 高cm
+ * @property int $is_huge 是否大件 0 否 1 是
+ * @property string $pd_weight 货物实际重量kg
+ * @property string $pd_throw_weight 抛重 长*宽*高/6000
+ * @property string $pd_count_weight 计算重量
+ * @property string $pd_material 材质
+ * @property int $pd_purchase_num 申请采购数量
+ * @property string $pd_pur_costprice 含税价格
+ * @property int $has_shipping_fee 是否含运 0 否 1 是
+ * @property string $bill_type 开票类型  --普票-- --专票--
+ * @property int $bill_tax_value 开票税率 --数字 并且小于16
+ * @property int $hs_code HS编码
+ * @property int $bill_tax_rebate 退税率
+ * @property string $bill_rebate_amount 退税金额
+ * @property string $no_rebate_amount 预计销售不退税价格RMB
+ * @property string $retail_price 预计销售价格$
+ * @property string $ebay_url eBay最低价链接
+ * @property string $amazon_url amazon最低价链接
+ * @property string $url_1688 1688最低价链接
+ * @property string $shipping_fee 海运运费预估
+ * @property string $oversea_shipping_fee 海外仓运运费预估
+ * @property string $transaction_fee 成交费 销售金额的13%
+ * @property string $gross_profit 预估毛利
+ * @property string $remark 备注
+ * @property int $parent_product_id 关联的母SKU ID
+ */
+class PurInfo extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'pur_info';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['pur_group', 'pd_purchase_num', 'hs_code','parent_product_id',], 'integer'],
+            [['pd_weight', 'pd_throw_weight', 'pd_count_weight', 'pd_pur_costprice'], 'number'],
+            [['pd_title', 'pd_title_en', 'remark'], 'string', 'max' => 100],
+            [['pd_pic_url', 'ebay_url', 'amazon_url', 'url_1688'], 'string', 'max' => 500],
+            [['pd_package', 'pd_material'], 'string', 'max' => 1000],
+            [['pur_responsible_id', 'pd_length', 'pd_width', 'pd_height', 'bill_type'], 'string', 'max' => 10],
+            [['is_huge', 'has_shipping_fee'], 'string', 'max' => 1],
+            [['bill_tax_value', 'bill_tax_rebate'], 'string', 'max' => 4],
+            [['bill_rebate_amount', 'no_rebate_amount', 'retail_price', 'shipping_fee', 'oversea_shipping_fee', 'transaction_fee', 'gross_profit'], 'string', 'max' => 30],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'pur_info_id' => 'ID',
+            'pur_responsible_id' => '负责人',
+            'pur_group' => '序号',
+            'pd_title' => '中文简称',
+            'pd_title_en' => '英文简称',
+            'pd_pic_url' => '图片',
+            'pd_package' => '外包装',
+            'pd_length' => '长(cm)',
+            'pd_width' => '长(cm)',
+            'pd_height' => '宽(cm)',
+            'is_huge' => '高(cm)',
+            'pd_weight' => '货物实际重量kg',
+            'pd_throw_weight' => '抛重',
+            'pd_count_weight' => '计算重量',
+            'pd_material' => '材质',
+            'pd_purchase_num' => '申请采购数量',
+            'pd_pur_costprice' => '含税价格',
+            'has_shipping_fee' => '是否含运',
+            'bill_type' => '开票类型',
+            'bill_tax_value' => '开票税率',
+            'hs_code' => 'HS编码',
+            'bill_tax_rebate' => '退税率',
+            'bill_rebate_amount' => '退税金额',
+            'no_rebate_amount' => '预计销售不退税价格RMB',
+            'retail_price' => '预计销售价格',
+            'ebay_url' => 'eBay最低价链接',
+            'amazon_url' => 'Amazon最低价链接',
+            'url_1688' => '1688最低价链接',
+            'shipping_fee' => '海运运费预估',
+            'oversea_shipping_fee' => '海外仓运运费预估',
+            'transaction_fee' => '成交费',
+            'gross_profit' => '预估毛利',
+            'remark' => '备注',
+            'parent_product_id' => '父ID',
+        ];
+    }
+}
