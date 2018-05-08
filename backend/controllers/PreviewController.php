00329bfbdd0a9fdfdd58affed7e6d905d9ac95fe
Jenny <m17721480297@163.com>
2018-05-08 10:58:00 +0800

    preview control add

---
+++ backend/controllers/PreviewController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+use backend\models\ProductSearch;
+
+
+class PreviewController extends Controller
+{
+    public function actionIndex()
+    {
+        $searchModel = new ProductSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+}
