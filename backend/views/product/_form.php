ae495145eef825b762dac819f4cde9e87db87798
Jenny <m17721480297@163.com>
2018-05-30 13:45:02 +0800

    some require column add span *

--- backend/views/product/_form.php
+++ backend/views/product/_form.php
@@ -1,8 +1,8 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
-use yii\helpers\Url;
+use kartik\widgets\ActiveForm;
+use kartik\builder\Form;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Product */
@@ -12,22 +12,40 @@ use yii\helpers\Url;
 <div class="product-form">
 
     <?php $form = ActiveForm::begin(); ?>
+    <?php
+            echo Form::widget([
+                'model'=>$model,
+                'form'=>$form,
+                'columns'=>2,
+                'contentBefore'=>'<legend class="text-info"><h3>1.基本信息</h3></legend>',
+                'attributes'=>[       // 3 column layout
+                    'product_title'=>['type'=>Form::INPUT_TEXT, 'options'=>['placeholder'=>'']],
+                    'product_title_en'=>['type'=>Form::INPUT_TEXT, 'options'=>['placeholder'=>'']],
+                    'product_purchase_value'=>['type'=>Form::INPUT_TEXT, 'options'=>['placeholder'=>'']],
+                    'pd_pic_url'=>['type'=>Form::INPUT_TEXT, 'options'=>['placeholder'=>'地址格式:https://XXXX.jpg|png|gif等']],
 
-    <?= $form->field($model, 'pd_pic_url')->textInput(['maxlength' => true]) ?>
+                ],
 
-    <?= $form->field($model, 'product_title')->textInput(['maxlength' => true]) ?>
+            ]);
+            echo Form::widget([
+                'model'=>$model,
+                'form'=>$form,
+                'columns'=>2,
+                'contentBefore'=>'<legend class="text-info"><h3>2.链接信息</h3></legend>',
+                'attributes'=>[       // 3 column layout
 
-    <?= $form->field($model, 'product_title_en')->textInput(['maxlength' => true]) ?>
+                    'ref_url1'=>['type'=>Form::INPUT_TEXT, 'options'=>['placeholder'=>'']],
+                    'ref_url2'=>['type'=>Form::INPUT_TEXT, 'options'=>['placeholder'=>'']],
+                    'ref_url3'=>['type'=>Form::INPUT_TEXT, 'options'=>['placeholder'=>'']],
+                    'ref_url4'=>['type'=>Form::INPUT_TEXT, 'options'=>['placeholder'=>'']],
 
-    <?= $form->field($model, 'product_purchase_value')->textInput(['maxlength' => true]) ?>
+                ],
 
-    <?= $form->field($model, 'ref_url1')->textInput(['maxlength' => true]) ?>
+            ]);
 
-    <?= $form->field($model, 'ref_url2')->textInput(['maxlength' => true]) ?>
+    ?>
 
-    <?= $form->field($model, 'ref_url3')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'ref_url4')->textInput(['maxlength' => true]) ?>
 
 <!--    --><?php //echo  $form->field($model, 'product_add_time')->textInput() ?>
 
@@ -55,6 +73,23 @@ $this->registerJs("
             $('.form-control').css('border-radius','7px')
         }); 
         ", \yii\web\View::POS_END);
+
+//span * 必填字段
+
+$require_js = <<<JS
+    $(function(){
+         $("label[for='product-product_title'] ").addClass("label-require");
+         $("label[for='product-product_title_en'] ").addClass("label-require");
+            
+            $('.label-require').html(function(_,html) {
+                return html.replace(/(.*?)/, "<span style = 'color:red'><big>*$1</big></span>");
+            });
+    });
+JS;
+
+$this->registerJs($require_js);
+
+
 
 
 ?>
