a483290966844fc2455737f7b6fe4c0eeb494518
Jenny <m17721480297@163.com>
2018-06-03 21:54:58 +0800

    gross_profit add junior

--- backend/views/junior-pur-info/update.php
+++ backend/views/junior-pur-info/update.php
@@ -121,6 +121,7 @@ $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
                 'oversea_shipping_fee'=>['type'=>Form::INPUT_TEXT, 'options'=>['placeholder'=>'']],
                 'transaction_fee'=>['type'=>Form::INPUT_TEXT, 'options'=>['placeholder'=>'']],
                 'gross_profit'=>['type'=>Form::INPUT_TEXT, 'options'=>['placeholder'=>'']],
+                'profit_rate'=>['type'=>Form::INPUT_TEXT, 'options'=>['placeholder'=>'']],
             ]
         ]);
 
@@ -189,6 +190,7 @@ $readonly_js =<<<JS
             $("#purinfo-oversea_shipping_fee").attr("readonly","readonly");
             $("#purinfo-transaction_fee").attr("readonly","readonly");
             $("#purinfo-gross_profit").attr("readonly","readonly");
+            $("#purinfo-profit_gross").attr("readonly","readonly");
             $("#purinfo-no_rebate_amount").attr("readonly","readonly");
             $("#purinfo-pur_group").attr("readonly","readonly");
             
@@ -298,6 +300,9 @@ $compute_js =<<<JS
             //含税价格 costprice
             gross_profit = (retail_price*$exchange_rate-costprice+-shipping_fee-oversea_fee-transaction_fee*$exchange_rate).toFixed(3) ;
             $("#purinfo-gross_profit").val(gross_profit);
+                 //毛利率
+            var profit_rate = (gross_profit*100/no_rebate_amount).toFixed(3);
+             $("#purinfo-profit_rate").val(profit_rate);
             
         });
 
