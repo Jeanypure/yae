f0a97993f8b63415938da145f651aa915c923cfe
Jenny <m17721480297@163.com>
2018-06-08 13:58:07 +0800

    pur-info-track

--- backend/views/pur-info-track/index.php
+++ backend/views/pur-info-track/index.php
@@ -1,19 +1,20 @@
 <?php
 
+
 use yii\helpers\Html;
-use yii\grid\GridView;
+use kartik\grid\GridView;
+use yii\helpers\Url;
+use kartik\daterange\DateRangePicker;
 
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\PurInfoTrackSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = Yii::t('app', 'Pur Infos');
+$this->title = Yii::t('app', '采样申请');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="pur-info-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
         <?= Html::a(Yii::t('app', 'Create Pur Info'), ['create'], ['class' => 'btn btn-success']) ?>
@@ -22,15 +23,123 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
+        'export' => false,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
+            ['class' => 'yii\grid\ActionColumn',
+                'header' => '操作',
+                'template' => '{view} {update}',
+            ],
 
-            'pur_info_id',
-//            'spur_info_id',
+
+
+//            'pur_info_id',
+            'spur_info_id',
+            [
+                'class' => 'yii\grid\Column',
+                'headerOptions' => [
+                    'width'=>'100'
+                ],
+                'header' => '图片',
+                'content' => function ($model, $key, $index, $column){
+                    return "<img src='" .$model->pd_pic_url. "' width='100' height='100'>";
+
+
+                }
+            ],
+
             'purchaser',
-            'pur_group',
-            'pd_title',
-            'pd_title_en',
+            [
+                'attribute'=>'pur_group',
+                'value' => function($model) {
+                    if($model->pur_group==1){
+                        return '一部';
+                    }elseif ($model->pur_group==2){
+                        return '二部';
+                    }elseif ($model->pur_group==3){
+                        return '三部';
+                    }elseif ($model->pur_group==4){
+                        return '四部';
+                    }elseif ($model->pur_group==5){
+                        return '五部';
+                    }elseif ($model->pur_group==6){
+                        return '六部';
+                    }elseif ($model->pur_group==7){
+                        return '七部';
+                    }elseif ($model->pur_group==8){
+                        return '八部';
+                    }
+                },
+                'contentOptions'=> ['style' => 'width: 10%; word-wrap: break-word;white-space:pre-line;'],
+                'format'=>'html',
+                'filterType'=>GridView::FILTER_SELECT2,
+                'filter'=>['1' => '一部', '2' => '二部','3' => '三部','4' => '四部','5' => '五部','6' => '六部','7' => '七部','8' => '八部',],
+                'filterWidgetOptions'=>[
+                    'pluginOptions'=>['allowClear'=>true],
+                ],
+                'filterInputOptions'=>['placeholder'=>'部门'],
+                'group'=>true,  // enable grouping
+
+            ],
+
+            [
+                'attribute'=>'pd_title',
+                'value' => function($model) { return $model->pd_title;},
+                'contentOptions'=> ['style' => 'width: 50%; word-wrap: break-word;white-space:pre-line;'],
+                'format'=>'html',
+                'headerOptions' => [
+                    'width'=>'80%'
+                ],
+            ],
+            [
+                'attribute'=>'pd_title_en',
+                'value' => function($model) { return $model->pd_title_en;},
+                'contentOptions'=> ['style' => 'width: 50%; word-wrap: break-word;white-space:pre-line;'],
+                'format'=>'html',
+                'headerOptions' => [
+                    'width'=>'80%'
+                ],
+            ],
+            [
+                'attribute'=>'master_result',
+                'value' => function($model) {
+                    if($model->master_result==0){
+                        return '拒绝';
+                    }elseif($model->master_result==1){
+                        return '采样';
+
+                    }elseif($model->master_result==2){
+                        return '需议价和谈其他条件';
+
+                    }elseif($model->master_result==3){
+                        return '尚未评';
+
+                    }elseif($model->master_result==4){
+                        return '直接下单';
+
+                    }elseif($model->master_result==5){
+                        return '季节产品推迟';
+
+                    }
+                },
+                'contentOptions'=> ['style' => 'width: 50%; word-wrap: break-word;white-space:pre-line;'],
+                'format'=>'html',
+                'filterType'=>GridView::FILTER_SELECT2,
+                'filter'=>['0' => '拒绝', '1' => '采样', '2' => '需议价和谈其他条件', '3' => '尚未评', '4' => '直接下单', '5' => '季节产品推迟'],
+                'filterWidgetOptions'=>[
+                    'pluginOptions'=>['allowClear'=>true],
+                ],
+                'filterInputOptions'=>['placeholder'=>'评审状态'],
+            ],
+            [
+                'attribute'=>'master_mark',
+                'value' => function($model) { return $model->master_mark;},
+                'contentOptions'=> ['style' => 'width: 50%; word-wrap: break-word;white-space:pre-line;'],
+                'format'=>'html',
+                'headerOptions' => [
+                    'width'=>'80%'
+                ],
+            ],
             //'pd_pic_url:url',
             //'pd_package',
             //'pd_length',
@@ -85,7 +194,6 @@ $this->params['breadcrumbs'][] = $this->title;
             //'is_assign',
             //'commit_date',
 
-            ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
 </div>
