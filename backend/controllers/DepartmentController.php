622e1e2b2827523f0311aaeeb9381b054b843c8b
Jenny <m17721480297@163.com>
2018-06-14 19:13:48 +0800

    purchaser role=4 or 5

--- backend/controllers/DepartmentController.php
+++ backend/controllers/DepartmentController.php
@@ -213,7 +213,7 @@ class DepartmentController extends Controller
                             p.`purchaser`,
                             0 as num         
                          from `purchaser` p 
-                         where p.`role`=0 or p.`role`=4
+                         where p.`role`=0 or p.`role`=4 or p.`role`=5
                          GROUP BY p.`purchaser` 
                          ")->queryAll();
 
