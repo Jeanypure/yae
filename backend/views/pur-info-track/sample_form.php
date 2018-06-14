fc1366a31427361a4dde7bac62c4a6fe3d8781e4
Jenny <m17721480297@163.com>
2018-06-14 14:10:14 +0800

    fee total

--- backend/views/pur-info-track/sample_form.php
+++ backend/views/pur-info-track/sample_form.php
@@ -21,23 +21,32 @@ use kartik\widgets\ActiveForm;
         'form'=>$form,
         'columns'=>6,
         'contentBefore'=>'<legend class="text-info"><h3>2.样品费用信息</h3></legend>',
-        'attributes'=>[       // 3 column layout
+        'attributes'=>[       // 6 column layout
             'procurement_cost'=>['type'=>Form::INPUT_TEXT, 'options'=>['placeholder'=>'']],
             'sample_freight'=>['type'=>Form::INPUT_TEXT,
                 'labelOptions'=>['class'=>'label-require'],
                 'options'=>['placeholder'=>'','class'=>'label-require']],
             'else_fee'=>['type'=>Form::INPUT_TEXT, 'options'=>['placeholder'=>'']],
             'pay_amount'=>['type'=>Form::INPUT_TEXT, 'options'=>['placeholder'=>'']],
+        ],
+
+    ]);
+    echo Form::widget([
+        'model'=>$model,
+        'form'=>$form,
+        'columns'=>1,
+        'attributes'=>[       // 6 column layout
+
             'pay_way'=>['type'=>Form::INPUT_TEXT, 'options'=>['placeholder'=>'']],
-            'mark'=>['type'=>Form::INPUT_TEXT, 'options'=>['placeholder'=>'']],
+            'mark'=>['type'=>Form::INPUT_TEXTAREA, 'options'=>['placeholder'=>'']],
             'fee_return'=>[
                 'type'=>Form::INPUT_RADIO_LIST,
+                'label'=>"<span style = 'color:red'><big>*</big></span>是否退样品费",
                 'items'=>[1=>'是', 0=>'否'],
                 'options'=>['placeholder'=>'',]
             ],
             'spur_info_id'=>['type'=>Form::INPUT_HIDDEN, 'options'=>['placeholder'=>'']],
 
-//            is_quality,fee_return,audit_mem1,audit_mem2,audit_mem3,applicant
         ],
 
     ]);
@@ -107,7 +116,52 @@ use kartik\widgets\ActiveForm;
     });
 
 JS;
-
     $this->registerJs($js);
+
+//css 表单input 变圆润
+
+$this->registerJs("
+        $(function () {
+            $('.form-control').css('border-radius','6px')
+        }); 
+        ", \yii\web\View::POS_END);
+$readonly_js =<<<JS
+        $(function(){
+            
+            $("#sample-pay_amount").attr("readonly","readonly");
+            
+          
+            $("label[for='sample-procurement_cost'] ").addClass("label-require");
+            $("label[for='sample-sample_freight'] ").addClass("label-require");
+            $("label[for='sample-pay_way'] ").addClass("label-require");
+
+            
+            $('.label-require').html(function(_,html) {
+                return html.replace(/(.*?)/, "<span style = 'color:red'><big>*$1</big></span>");
+            });
+                
+         
+
+        });
+        
+JS;
+$this->registerJs($readonly_js);
+
+//费用计算
+
+$compute_js = <<<JS
+         $('#w2').on('change',function() {
+            // 付款金额 = 采购成本 + 运费 + 其他费用 
+            var pay_amount;
+            var procurement_cost  = $('#sample-procurement_cost').val();
+            var sample_freight  = $('#sample-sample_freight').val();
+            pay_amount = parseFloat(procurement_cost) + parseFloat(sample_freight);
+                // console.log(pay_amount);
+                $('#sample-pay_amount').val(pay_amount)
+         });
+JS;
+
+$this->registerJs($compute_js);
+
 ?>
 
