311fa2aee43eec0acad619fdd2cbe06d2b56a6c8
unknown <wakakajingjing@gmail.com>
2018-06-26 16:39:45 +0800

    task found

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -1,5 +1,16 @@
 <?php
 use mdm\admin\components\MenuHelper;
+
+$sql = "select
+'经理评审' as task_name,
+count(*) as task_num
+FROM pur_info WHERE audit_a=1 AND audit_b=1 AND preview_status=0 ";
+$status_result = yii::$app->db->createCommand($sql)->queryAll();
+$status_map = [];
+foreach ($status_result as $res){
+    $status_map[$res['task_name']] = $res['task_num'];
+}
+
 $callback = function($menu){
     $data = json_decode($menu['data'], true);
     $items = $menu['children'];
@@ -41,4 +52,15 @@ $callback = function($menu){
     </section>
 
 </aside>
+
+<?php
+//注册JS 为每个模块加数量
+$JS = <<< JS
+//找到产品推荐模块并追加<span>
+    $("a span:contains('经理评审')").after('<sup class="label label-info">{$status_map["经理评审"]}</sup>');
+    
+JS;
+$this->registerJs($JS);
+
+?>
 
