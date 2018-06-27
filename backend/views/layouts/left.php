14b893be662b872e7c1fef268161f45c60041cb6
unknown <wakakajingjing@gmail.com>
2018-06-27 15:59:42 +0800

    task remind

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -1,10 +1,13 @@
 <?php
 use mdm\admin\components\MenuHelper;
 
-$sql = "select
-'经理评审' as task_name,
-count(*) as task_num
-FROM pur_info WHERE audit_a=1 AND audit_b=1 AND preview_status=0 ";
+//$sql = "select
+//'经理评审' as task_name,
+//count(*) as task_num
+//FROM pur_info WHERE audit_a=1 AND audit_b=1 AND preview_status=0 ";
+$username = Yii::$app->user->identity->username;
+
+$sql = "CALL proc_SelectTaskNum ('$username')";
 $status_result = yii::$app->db->createCommand($sql)->queryAll();
 $status_map = [];
 foreach ($status_result as $res){
@@ -57,7 +60,20 @@ $callback = function($menu){
 //注册JS 为每个模块加数量
 $JS = <<< JS
 //找到产品推荐模块并追加<span>
+    $("a span:contains('销售推荐')").after('<sup class="label label-info">{$status_map["销售推荐"]}</sup>');
+    $("a span:contains('产品分部1')").after('<sup class="label label-info">{$status_map["产品分部1"]}</sup>');
+    $("a span:contains('公示列表1')").after('<sup class="label label-info">{$status_map["公示列表1"]}</sup>');
+    $("a span:contains('产品分部2')").after('<sup class="label label-info">{$status_map["产品分部2"]}</sup>');
+    $("a span:contains('公示列表2')").after('<sup class="label label-info">{$status_map["公示列表2"]}</sup>');
+    $("a span:contains('产品评审')").after('<sup class="label label-info">{$status_map["产品评审"]}</sup>');
     $("a span:contains('经理评审')").after('<sup class="label label-info">{$status_map["经理评审"]}</sup>');
+    $("a span:contains('采购开发')").after('<sup class="label label-info">{$status_map["采购开发"]}</sup>');
+    $("a span:contains('推送产品开发')").after('<sup class="label label-info">{$status_map["推送产品开发"]}</sup>');
+    
+    $("a span:contains('拿样制单')").after('<sup class="label label-info">{$status_map["拿样制单"]}</sup>');
+    $("a span:contains('部长审批')").after('<sup class="label label-info">{$status_map["部长审批"]}</sup>');
+    $("a span:contains('财务付款')").after('<sup class="label label-info">{$status_map["财务付款"]}</sup>');
+
     
 JS;
 $this->registerJs($JS);
