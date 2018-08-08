2dd242754d10629f1769a857238d9c968cf6d087
unknown <wakakajingjing@gmail.com>
2018-08-08 15:23:48 +0800

    add role view

--- backend/models/CommissionSearch.php
+++ backend/models/CommissionSearch.php
@@ -25,9 +25,26 @@ class CommissionSearch extends PurInfo
     }
     public function Search($params)
     {
-
-        $query = PurInfo::find()->alias('po')
-            ->select(["po.`pur_info_id`,po.`pur_group`,po.`source`,
+        $username = Yii::$app->user->identity->username;
+        $userid = Yii::$app->user->identity->getId();
+        $getRolesByUser = Yii::$app->authManager->getRolesByUser($userid);
+        $res = Company::find()->select('id,sub_company')
+            ->where("leader_id=".$userid)->asArray()->one();
+        $sub_id = $res['id']??'';
+        $userrole = '';
+        foreach ($getRolesByUser as $key=>$val){
+            if($key != 'guest'){
+             $userrole = $key;
+            }
+        }
+        /*1 推送产品  部长 权重5 采购权重7  不乘等级
+        2 部长看自己部门产品
+        3 采购自己名下产品
+        4 审核组审核所有产品
+        */
+        if($userrole == '审核组'||$userrole == '超级管理员'){
+            $query = PurInfo::find()->alias('po')
+                ->select(["po.`pur_info_id`,po.`pur_group`,po.`source`,
                 po.`pd_pic_url`,po.`purchaser`,po.`is_purchase`,po.`pd_pur_costprice`,
                 e.`has_arrival`,e.`write_date`,e.`minister_result`,
                 CASE  WHEN po.`pd_pur_costprice` > 150 THEN 500
@@ -39,11 +56,50 @@ class CommissionSearch extends PurInfo
                 ELSE 0 END AS 'weight',
                 pr.`grade` 
                 "])
-            ->joinWith('sample as e')
-            ->leftJoin('purchaser AS pr','pr.purchaser = po.purchaser')
-            ->andWhere(['po.is_purchase'=>1])
-            ->orderBy('po.pur_info_id desc')
-        ;
+                ->joinWith('sample as e')
+                ->leftJoin('purchaser AS pr','pr.purchaser = po.purchaser')
+                ->andWhere(['po.is_purchase'=>1])
+                ->orderBy('po.pur_info_id desc');
+            ;
+        }elseif ($userrole == '销售部长'){
+            $query = PurInfo::find()->alias('po')
+                ->select(["po.`pur_info_id`,po.`pur_group`,po.`source`,
+                po.`pd_pic_url`,po.`purchaser`,po.`is_purchase`,po.`pd_pur_costprice`,
+                e.`has_arrival`,e.`write_date`,e.`minister_result`,
+                CASE  WHEN po.`pd_pur_costprice` > 150 THEN 500
+                ELSE 400 END AS 'unit_price',    
+                CASE WHEN e.`minister_result`=0 THEN 0
+                     WHEN e.`minister_result`=1   THEN '5'
+                     WHEN e.`minister_result`=2   THEN '10'
+                     WHEN e.`minister_result`=3   THEN '5'
+                ELSE 0 END AS 'weight',
+                pr.`grade` 
+                "])
+                ->joinWith('sample as e')
+                ->leftJoin('purchaser AS pr','pr.purchaser = po.purchaser')
+                ->andWhere(['po.is_purchase'=>1])
+                ->orderBy('po.pur_info_id desc');
+            $this->pur_group = $sub_id;
+        }else{ //采购
+            $query = PurInfo::find()->alias('po')
+                ->select(["po.`pur_info_id`,po.`pur_group`,po.`source`,
+                po.`pd_pic_url`,po.`purchaser`,po.`is_purchase`,po.`pd_pur_costprice`,
+                e.`has_arrival`,e.`write_date`,e.`minister_result`,
+                CASE  WHEN po.`pd_pur_costprice` > 150 THEN 500
+                ELSE 400 END AS 'unit_price',    
+                CASE WHEN e.`minister_result`=0 THEN 0
+                     WHEN e.`minister_result`=1   THEN '5'
+                     WHEN e.`minister_result`=2   THEN '10'
+                     WHEN e.`minister_result`=3   THEN '7'
+                ELSE 0 END AS 'weight',
+                pr.`grade` 
+                "])
+                ->joinWith('sample as e')
+                ->leftJoin('purchaser AS pr','pr.purchaser = po.purchaser')
+                ->andWhere(['po.is_purchase'=>1])
+                ->orderBy('po.pur_info_id desc');
+            $this->purchaser = $username;
+        }
 
 
 
