3124956e7c52d4cefee6cab65ee9783aee39be4f
unknown <wakakajingjing@gmail.com>
2018-09-03 14:27:24 +0800

    del into winit_api

--- backend/controllers/WinitController.php
+++ backend/controllers/WinitController.php
@@ -49,10 +49,10 @@ class WinitController  extends \yii\base\Controller
             //先转成json字符串，再替换！！
             $body = str_replace('[]','{}',$body);
 
-            $request_date = date('Y-m-d H:m:s');
-            Yii::$app->db->createCommand("
+           /* $request_date = date('Y-m-d H:m:s');*/
+            /*Yii::$app->db->createCommand("
              insert into winit_api (`url`,`action`,`request_date`) values ('$base_url','{$body}','$request_date')
-             ")->execute();
+             ")->execute();*/
             return $this->actionDoCurlPostRequest($base_url, $body);
         }
 
