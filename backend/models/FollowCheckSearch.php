177fc8eb0d5a737194dfa6a4af46ba4fb296dbf0
Pure <wakakajingjing@gmail.com>
2018-09-09 13:59:47 +0800

    val username

--- backend/models/FollowCheckSearch.php
+++ backend/models/FollowCheckSearch.php
@@ -50,10 +50,11 @@ class FollowCheckSearch extends PurInfo
             'lvy'=> '2,6',
             'Belle'=> '3,4',
             'Yilia'=> '1'];
-        $val = $user_dict[$username];
+
         if ($username =='Jenny'||$username =='David'||$username =='Mark' ){
             $query = PurInfo::find()->andWhere(['is_purchase'=>1])->orderBy('pur_info_id desc');
         }else{
+            $val = $user_dict[$username];
             $query = PurInfo::find()
                 ->Where('pur_group IN('.$val.')')
                 ->andWhere(['is_purchase'=>1])
