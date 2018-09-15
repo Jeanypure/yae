56974d010ccdb205f704fe308588998817ec1af7
Pure <wakakajingjing@gmail.com>
2018-09-15 12:05:59 +0800

    search pd_sku

--- backend/models/DepartmentSelfSearch.php
+++ backend/models/DepartmentSelfSearch.php
@@ -19,7 +19,7 @@ class DepartmentSelfSearch extends PurInfo
     {
         return [
             [['pur_info_id', 'pur_group', 'is_huge', 'pd_purchase_num', 'has_shipping_fee',  'hs_code', 'bill_tax_rebate', 'parent_product_id'], 'integer'],
-            [['master_result','master_mark','purchaser', 'pd_title', 'pd_title_en', 'pd_pic_url', 'pd_package', 'pd_length', 'pd_width', 'pd_height', 'pd_material', 'bill_type', 'bill_rebate_amount', 'no_rebate_amount', 'retail_price', 'ebay_url', 'amazon_url', 'url_1688', 'shipping_fee', 'oversea_shipping_fee', 'transaction_fee', 'gross_profit', 'remark', 'source', 'member', 'preview_status', 'brocast_status'], 'safe'],
+            [['pd_sku','master_result','master_mark','purchaser', 'pd_title', 'pd_title_en', 'pd_pic_url', 'pd_package', 'pd_length', 'pd_width', 'pd_height', 'pd_material', 'bill_type', 'bill_rebate_amount', 'no_rebate_amount', 'retail_price', 'ebay_url', 'amazon_url', 'url_1688', 'shipping_fee', 'oversea_shipping_fee', 'transaction_fee', 'gross_profit', 'remark', 'source', 'member', 'preview_status', 'brocast_status'], 'safe'],
             [['pd_weight', 'pd_throw_weight', 'pd_count_weight', 'pd_pur_costprice'], 'number'],
         ];
     }
@@ -81,6 +81,7 @@ class DepartmentSelfSearch extends PurInfo
             'parent_product_id' => $this->parent_product_id,
             'preview_status' => $this->preview_status,
             'brocast_status' => $this->brocast_status,
+            'pd_sku' => $this->pd_sku,
         ]);
 
         $query->andFilterWhere(['like', 'purchaser', $this->purchaser])
