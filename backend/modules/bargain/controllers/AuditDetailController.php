12ae62d37cdb3233828cc6e01776bd30f0327e25
Jenny <m17721480297@163.com>
2018-12-01 00:03:29 +0800

    put vendor

--- backend/modules/bargain/controllers/AuditDetailController.php
+++ backend/modules/bargain/controllers/AuditDetailController.php
@@ -164,11 +164,11 @@ class AuditDetailController extends Controller
         try{
 
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
                 'Authorization: NLAuth nlauth_account=5151251, nlauth_email=jenny.li@yaemart.com, nlauth_signature=Jenny666666, nlauth_role=1047',
                 'Content-Type: application/json',
                 'Accept: application/json'
-            ));
+            ]);
             curl_setopt($ch, CURLOPT_POST, 1);
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
             curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,2);
@@ -206,18 +206,23 @@ class AuditDetailController extends Controller
         $vendorItem['fields'][] = $fields;
 
         $url2 = "https://5151251.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=154&deploy=2";
-        $ret = $this->actionDoCurl($vendorItem,$url2);
-
+        $ret = $this->actionPuturl($vendorItem,$url2);
+        return json_decode($ret,true);
 
     }
 
-    function actionPuturl($url,$data){
+    function actionPuturl($data,$url){
         $data = json_encode($data);
         $ch = curl_init(); //初始化CURL句柄
-        curl_setopt ($ch, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
+        curl_setopt ($ch, CURLOPT_HTTPHEADER,[
+            'Authorization: NLAuth nlauth_account=5151251, nlauth_email=jenny.li@yaemart.com, nlauth_signature=Jenny666666, nlauth_role=1047',
+            'Content-Type: application/json',
+            'Accept: application/json'
+        ]);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,1); //设为TRUE把curl_exec()结果转化为字串，而不是直接输出
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST,"PUT"); //设置请求方式
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);//设置提交的字符串
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data,true));//设置提交的字符串
         $output = curl_exec($ch);
         curl_close($ch);
         return json_decode($output,true);
