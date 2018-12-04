d44c0bad306a151bb1eadfd300fba1a4e58a753c
Pure <wakakajingjing@gmail.com>
2018-12-04 16:29:35 +0800

    audit bargain price

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -72,7 +72,8 @@ $JS = <<< JS
     $("a span:contains('拿样制单')").after('<sup class="label label-info">{$status_map["拿样制单"]}</sup>');
     $("a span:contains('部长审批')").after('<sup class="label label-warning">{$status_map["部长审批"]}</sup>');
     $("a span:contains('财务付款')").after('<sup class="label label-success">{$status_map["财务付款"]}</sup>');
-    $("a span:contains('议价产品列表')").after('<sup class="label label-success">{$status_map["议价产品列表"]}</sup>');
+    $("a span:contains('议价产品列表')").after('<sup class="label label-primary">{$status_map["议价产品列表"]}</sup>');
+    $("a span:contains('审核议价产品')").after('<sup class="label label-success">{$status_map["审核议价产品"]}</sup>');
 
     
 JS;
