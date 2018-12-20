1d374627ac02f81ad1e203b10aaf454d706fc5db
Pure <wakakajingjing@gmail.com>
2018-12-20 10:58:43 +0800

    sample replace by pur_info

--- backend/models/PurInfoTrackSearch.php
+++ backend/models/PurInfoTrackSearch.php
@@ -52,9 +52,9 @@ class PurInfoTrackSearch extends PurInfo
                     `pur_info`.pur_info_id,
                     `pur_info`.pd_title,`pur_info`.pd_title_en,`pur_info`.purchaser,`pur_info`.pd_pic_url,
                     `pur_info`.pur_group,`pur_info`.master_result,`pur_info`.master_mark,
-                   `sample`.sample_submit1,`pur_info`.is_quality,`pur_info`.submit1_at,
-                   `sample`.payer,`pur_info`.has_pay,`pur_info`.pay_at,`pur_info`.submit2_at,`pur_info`.sample_return,
-                   `sample`.is_purchase,
+                   `pur_info`.sample_submit1,`pur_info`.is_quality,`pur_info`.submit1_at,
+                   `pur_info`.payer,`pur_info`.has_pay,`pur_info`.pay_at,`pur_info`.submit2_at,`pur_info`.sample_return,
+                   `pur_info`.is_purchase,
                     `sample`.write_date,`sample`.spur_info_id,`sample`.create_date,
                     `sample`.is_agreest,`sample`.pd_sku,`sample`.purchaser_result'])
                 ->joinWith('sample')
@@ -69,9 +69,9 @@ class PurInfoTrackSearch extends PurInfo
                     `pur_info`.pur_info_id,
                     `pur_info`.pd_title,`pur_info`.pd_title_en,`pur_info`.purchaser,`pur_info`.pd_pic_url,
                     `pur_info`.pur_group,`pur_info`.master_result,`pur_info`.master_mark,
-                    `sample`.sample_submit1,`pur_info`.is_quality,`pur_info`.submit1_at,
-                   `sample`.payer,`pur_info`.has_pay,`pur_info`.pay_at,`pur_info`.submit2_at,`pur_info`.sample_return,
-                   `sample`.is_purchase,
+                     `pur_info`.sample_submit1,`pur_info`.is_quality,`pur_info`.submit1_at,
+                   `pur_info`.payer,`pur_info`.has_pay,`pur_info`.pay_at,`pur_info`.submit2_at,`pur_info`.sample_return,
+                   `pur_info`.is_purchase,
                     `sample`.write_date, `sample`.spur_info_id,`sample`.create_date,`sample`.is_agreest,`sample`.pd_sku,
                     `sample`.purchaser_result'])
                 ->joinWith('sample')
@@ -82,8 +82,8 @@ class PurInfoTrackSearch extends PurInfo
                 ->orderBy('pur_info_id desc')
             ;
         }
-//        $this->sample_submit1 = 0;
-
+        $this->sample_submit1 = 0;
+//
 //            echo  $query->createCommand()->getRawSql();die;
         // add conditions that should always apply here
 
