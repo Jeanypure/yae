6cfea1afadc38fcb4b6ec48fb67d17251eb542fa
Pure <wakakajingjing@gmail.com>
2019-02-12 17:56:48 +0800

    add DW company

--- backend/views/department-debit/index.php
+++ backend/views/department-debit/index.php
@@ -116,7 +116,8 @@ $this->params['breadcrumbs'][] = $this->title;
             [
                 'attribute'=>'receiver',
                 'value' => function($model) {
-                    $company = [ '1' => '大森林', '2' => '珑瑗', '3' => '昊宏', '4' => '安泰克', '5' => '文鼎','6'=>'龙辕','7'=>'瀚明'];
+                    $company = [ '1' => '大森林', '2' => '珑瑗', '3' => '昊宏', '4' => '安泰克', '5' => '文鼎','6'=>'龙辕',
+                        '7'=>'瀚明','8'=>'德威'];
                     return $company[$model->receiver];
                 },
                 'contentOptions'=> ['style' => 'width: 50%; word-wrap: break-word;white-space:pre-line;'],
@@ -125,7 +126,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     'width'=>'80%'
                 ],
                 'filterType'=>GridView::FILTER_SELECT2,
-                'filter'=>[ '1' => '大森林', '2' => '珑瑗', '3' => '昊宏', '4' => '安泰克', '5' => '文鼎','6'=>'龙辕','7'=>'瀚明'],
+                'filter'=>[ '1' => '大森林', '2' => '珑瑗', '3' => '昊宏', '4' => '安泰克', '5' => '文鼎','6'=>'龙辕','7'=>'瀚明','8'=>'德威'],
                 'filterWidgetOptions'=>[
                     'pluginOptions'=>['allowClear'=>true],
                 ],
