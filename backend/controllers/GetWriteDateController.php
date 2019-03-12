9be2718066182bafd4244e75dfb05afe516481e1
Pure <wakakajingjing@gmail.com>
2019-03-12 11:39:17 +0800

    write-date two btn add

--- backend/controllers/GetWriteDateController.php
+++ backend/controllers/GetWriteDateController.php
@@ -2,8 +2,7 @@
 
 namespace backend\controllers;
 
-
-
+use Yii;
 use backend\modules\bargain\controllers\ObtainDataController;
 
 class GetWriteDateController extends \yii\web\Controller
@@ -19,28 +18,60 @@ class GetWriteDateController extends \yii\web\Controller
     public function  actionGetReceipt(){
         $url = 'https://5151251.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=284&deploy=1';
         $result = ObtainDataController::actionDoCurl($url);
-        echo 123;
-//        echo json_encode($result);
+        $res_arr = json_decode($result,true);
+        $record_set = [];
+        if(is_array($res_arr)){
+            foreach ($res_arr as $key=>$value){
+                $record = [];
+                $record[] = $value['trandate'];
+                $record[] = $value['tranid'];
+                $record[] = $value['potext'];
+                $record[] = $value['itemsku'];
+                $record[] = $value['itemdescription'];
+                $record[] = $value['quantity'];
+                $record_set[] = $record;
+                unset($record);
+            }
+        }
+        $table = 'tb_item_receipt';
+        $column = ['receipt_date','receipt_no','purchase_no','sku','item_name','quantity'];
+        Yii::$app->db->createCommand('truncate table tb_item_receipt')->execute();
+        $res = Yii::$app->db->createCommand()->batchInsert($table,$column,$record_set)->execute();
+        return json_encode($res,true);
     }
 
-    public  function actionDoCurl($url)
-    {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_HTTPHEADER,[
-            'Authorization: NLAuth nlauth_account=5151251, nlauth_email=jenny.li@yaemart.com, nlauth_signature=Jenny666666, nlauth_role=1013',
-            'Content-Type: application/json',
-            'Accept: application/json'
-        ]);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,2);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
-        curl_setopt($ch, CURLOPT_URL, $url);
-        ob_start();
-        curl_exec($ch);
-        $result = ob_get_contents();
-        ob_end_clean();
-        curl_close($ch);
-        return $result;
+    /**
+     * 获取采购明细
+     */
+    public function  actionGetPurchaseOrder(){
+       $url = 'https://5151251.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=289&deploy=1';
+       $result = ObtainDataController::actionDoCurl($url);
+       $res_arr = json_decode($result,true);
+       $record_set = [];
+       if(is_array($res_arr)){
+           foreach ($res_arr as $key=>$value){
+               $record =[];
+               $record[] = $value['trandate'];
+               $record[] = $value['tranid'];
+               $record[] = $value['itemname'];
+               $record[] = $value['quantityshiprecv'];
+               $record[] = $value['quantitybilled'];
+               $record[] = $value['quantity'];
+               $record[] = $value['custcol_invoice_item_name'];
+               $record[] = $value['subsidiary'];
+               $record[] = $value['department'];
+               $record_set[] = $record;
+               unset($record);
+           }
+       }
+       $table = 'tb_purchase_detail';
+       $column = ['purchase_date','purchase_order_no','sku','receipt_quantity','requisition_quantity',
+           'purchase_quantity','purchaser','subsidiary','department'];
+       Yii::$app->db->createCommand('truncate table tb_purchase_detail')->execute();
+       $res = Yii::$app->db->createCommand()->batchInsert($table,$column,$record_set)->execute();
+       return json_encode($res,true);
     }
+
+
 }
 
--- backend/views/get-write-date/index.php
+++ backend/views/get-write-date/index.php
@@ -9,7 +9,7 @@ use yii\helpers\Url;
 
 <p>
     <?php echo Html::button('同步到货清单',['class' => 'btn btn-info' ,'id'=>'receipt'])?>
-    <?php echo Html::button('同步采购明细',['class' => 'btn btn-primary' ,'id'=>'purchase-detail'])?>
+    <?php echo Html::button('同步采购明细',['class' => 'btn btn-primary' ,'id'=>'purchase'])?>
 </p>
 <?php
 $hide_url = <<<JS
@@ -23,14 +23,28 @@ $this->registerJs($hide_url);
 
 <?php
 $receipt_url = Url::toRoute(['get-receipt']);
+$purchase_url = Url::toRoute(['get-purchase-order']);
 $request_receipt = <<<JS
     $(function(){
         $('#receipt').on('click',function() {
-            console.log(123);
           var button = $(this);
           button.attr('disabled','disabled');
           $.ajax({
           url: "{$receipt_url}",
+          type: 'GET',
+          success:function(res) {
+                alert(res);            
+          button.attr('disabled',false);
+          }
+          });
+        });
+        //同步采购订单
+         $('#purchase').on('click',function() {
+             console.log(666);
+          var button = $(this);
+          button.attr('disabled','disabled');
+          $.ajax({
+          url: "{$purchase_url}",
           type: 'GET',
           success:function(res) {
                 alert(res);            
