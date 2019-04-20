4e6d0811065e9f455f07139b53c755d209931332
Pure <wakakajingjing@gmail.com>
2019-04-20 11:53:33 +0800

    bill img save

--- backend/models/Goodssku.php
+++ backend/models/Goodssku.php
@@ -66,7 +66,7 @@ class Goodssku extends yii\db\ActiveRecord
             [['pd_title', 'pd_title_en'], 'string', 'max' => 300],
             [['pd_length','pd_width','pd_height','pd_weight','pd_creator',], 'string', 'max' =>10 ],
             [['image_url'], 'string', 'max' =>500 ],
-            [['audit_content','sale_company','sku_create_date','sku_update_date' ], 'safe'],
+            [['audit_content','sale_company','sku_create_date','sku_update_date','bill_img1_name_unit' ], 'safe'],
             [['sku','pd_title','pd_title_en','image_url','is_quantity_check','pd_costprice','declared_value','contain_battery',
                 'pd_length','pd_width','pd_height','pd_weight','sale_company','hs_code',
                 'declaration_item_key1','declaration_item_key2','declaration_item_key3','declaration_item_key4','declaration_item_key5',
