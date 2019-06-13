e64bd5d1f8dd3485effcb32d806331825e4090a0
Pure <wakakajingjing@gmail.com>
2019-06-13 11:37:50 +0800

    recordType list change in yaeSupplier

--- backend/views/yae-supplier/index.php
+++ backend/views/yae-supplier/index.php
@@ -140,18 +140,13 @@ $this->params['breadcrumbs'][] = $this->title;
             [
                 'attribute'=>'bill_type',
                 'value' => function($model) {
-                    if($model->bill_type==0){
-                        return '16%专票';
-                    }elseif ($model->bill_type==1){
-                        return '3%专票';
-                    }elseif ($model->bill_type==2){
-                        return '增值税普通发票';
-                    }
+                    $billSet = [ '13%专票','增值税普通发票','3%专票' ];
+                    return $billSet[$model->bill_type];
                 },
                 'contentOptions'=> ['style' => 'width: 50%; word-wrap: break-word;white-space:pre-line;'],
                 'format'=>'html',
                 'filterType'=>GridView::FILTER_SELECT2,
-                'filter'=>['0' => '16%专票','1' => '3%专票','2' => '增值税普通发票', ],
+                'filter'=>[ '13%专票','增值税普通发票','3%专票' ],
                 'filterWidgetOptions'=>[
                     'pluginOptions'=>['allowClear'=>true],
                 ],
@@ -165,19 +160,8 @@ $this->params['breadcrumbs'][] = $this->title;
             [
                 'attribute'=>'pay_cycleTime_type',
                 'value' => function($model) {
-                    if($model->pay_cycleTime_type==1){
-                        return '日结';
-                    }elseif ($model->pay_cycleTime_type==2){
-                        return '周结';
-                    }elseif ($model->pay_cycleTime_type==3){
-                        return '半月结';
-                    }elseif ($model->pay_cycleTime_type==4){
-                        return '月结';
-                    }elseif ($model->pay_cycleTime_type==5){
-                        return '隔月结';
-                    }elseif ($model->pay_cycleTime_type==6){
-                        return '其他';
-                    }
+                    $payType = ['1' => '日结', '2' => '周结','3' => '半月结','4' => '月结','5' => '隔月结','0' => '其他'];
+                    return $payType[$model->pay_cycleTime_type];
                 },
                 'contentOptions'=> ['style' => 'width: 50%; word-wrap: break-word;white-space:pre-line;'],
                 'format'=>'html',
@@ -192,19 +176,8 @@ $this->params['breadcrumbs'][] = $this->title;
             [
                 'attribute'=>'account_type',
                 'value' => function($model) {
-                    if($model->account_type==1){
-                        return '货到付款';
-                    }elseif ($model->account_type==2){
-                        return '款到发货';
-                    }elseif ($model->account_type==3){
-                        return '周期结算';
-                    }elseif ($model->account_type==4){
-                        return '售后付款';
-                    }elseif ($model->account_type==5){
-                        return '默认方式';
-                    }elseif ($model->account_type==6){
-                        return '其他';
-                    }
+                    $accountType = ['1' => '日结', '2' => '周结','3' => '半月结','4' => '月结','5' => '隔月结','0' => '其他'];
+                    return $accountType[$model->account_type];
                 },
                 'contentOptions'=> ['style' => 'width: 50%; word-wrap: break-word;white-space:pre-line;'],
                 'format'=>'html',
